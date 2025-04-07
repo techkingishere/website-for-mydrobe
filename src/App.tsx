63229a9fc3383fe550b5c39e371ff1acaeb80448
techkingishere <emirberaarslan@gmail.com>
2025-04-07 02:41:05 -0400

    fix: Remove unused React import and set jsxImportSource

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Routes, Route, Navigate, Link } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import AppLayout from './layouts/AppLayout';
